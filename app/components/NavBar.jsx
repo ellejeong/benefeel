f8d8f338eb38e7e07d8198d3147927f7ccd0eef3
mlkiung <meredith.kiung@gmail.com>
2017-03-03 12:57:43 -0500

    cleaned up main.jsx by moving onEnter functions out of routes...also moved loading all products from all products view to onAppEnter

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -10,7 +10,6 @@ export default class NavBar extends Component {
 			<div>
 				<nav className="navbar navbar-default">
 				<div className="container-fluid">
-
 					<div className="navbar-header">
 					<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 						<span className="sr-only">Toggle navigation</span>
--- app/main.jsx
+++ app/main.jsx
@@ -2,14 +2,15 @@
 import React from 'react'
 import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
 import {render} from 'react-dom'
-import {connect, Provider} from 'react-redux'
+import { connect, Provider } from 'react-redux'
+import axios from 'axios';
 
 import store from './store'
 import Jokes from './components/Jokes'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 import singleProduct from './components/singleProduct';
-import { receiveProduct, receiveAllProducts } from './action-creators/product';
+import { receiveProduct, receiveAllProducts, selectAllProducts } from './action-creators/product';
 import Products from './components/Products';
 import NavBar from './components/NavBar';
 
@@ -31,26 +32,26 @@ const ExampleApp = connect(
   </div>
 )
 
+const onAppEnter = () => {
+  axios.get('/api/products')
+    .then(products => {
+      store.dispatch(selectAllProducts(products.data));
+    })
+    .catch();
+};
+
+const onProductEnter = nextState => {
+  store.dispatch(receiveProduct(nextState.params.productId));
+};
+
 render(
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={ExampleApp}>
+      <Route path="/" component={ExampleApp} onEnter={onAppEnter}>
         <IndexRedirect to="/products" />
-        <Route
-          path="/products"
-          component={Products}
-          onEnter={(nextState) => {
-            store.dispatch(receiveAllProducts(nextState.params.products));
-          }}
-        />
+        <Route path="/products" component={Products} />
         <Route path="/Jokes" component={Jokes} />
-        <Route
-          path="/products/:productId"
-          component={singleProduct}
-          onEnter={(nextState) => {
-            store.dispatch(receiveProduct(nextState.params.productId));
-          }}
-        />
+        <Route path="/products/:productId" component={singleProduct} onEnter={onProductEnter} />
       </Route>
     </Router>
   </Provider>,
